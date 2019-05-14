89ef5c09e605d189065de9aaa7a887d3b39adaf1
Anquilis <43358398+Anquilis@users.noreply.github.com>
2019-05-13 20:51:37 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$("button").click(function(){
+    $("button").hide();
+    $("body").css("background-image","url(https://upload.wikimedia.org/wikipedia/commons/a/a8/Windows_XP_BSOD.png)");
+    //var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3%27); 
+    //audio.play();//
+});
+
+$("#snap2").mouseover(function(){
+    $("#snap2").text("Ś̶̨̟̘̙̥ͅn̴̺̣̱̘̯̞̞̱̉a̷̧̛̲͙̫̖̔͗̈́͝p̸̮̝̮̈");
+    $("snap").css("width","100px");
+    $("#snap").hide();
+});
+
